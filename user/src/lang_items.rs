e97f28658421e13b61f5a0c6210c0ab77b36b449
Jiang Sheng <jiangshengdev@outlook.com>
2022-05-22 11:26:40 +0800

    Refactor

--- user/src/lang_items.rs
+++ user/src/lang_items.rs
@@ -1,5 +1,7 @@
+use core::panic::PanicInfo;
+
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
     let err = info.message().unwrap();
 
     if let Some(location) = info.location() {
